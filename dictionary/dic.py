4d246eaaf30bb851defb50cdb826922de5995b7d
Suryabhan Singh <suryabhan283808@gmail.com>
2025-05-13 19:57:28 +0530

    Add additional examples for dictionary operations and type checks

--- dictionary/dic.py
+++ dictionary/dic.py
@@ -18,3 +18,13 @@ print(len(d)) # prints 7
 print(max(d)) # prints 34
 print(min(d)) # prints 'B.E'
 print(sum(d)) # prints TypeError: sum() of dictionary is not supported
+print(type(d)) # prints <class 'dict'>
+print(id)
+
+e={ 2:'python', 2:37, '3':'B.E'} # prints {2: 37, '3': 'B.E'} over loading
+print(e) # prints {2: 37, '3': 'B.E'}
+print(sum(e)) # prints TypeError: sum() of dictionary is not supported
+print(max(e)) # prints 37
+print(min(e)) # prints 'B.E'   
+print(len(e)) # prints 2
+print(type(e)) # prints <class 'dict'>  
