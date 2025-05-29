a934ee2c927340b0e67342abd4122ef2c1703efc
Suryabhan Singh <suryabhan283808@gmail.com>
2025-05-29 20:15:27 +0530

    Refactor example code for map function and add multiple list addition example

--- Higher_order_Function/highrtOrder.py
+++ Higher_order_Function/highrtOrder.py
@@ -11,9 +11,18 @@
 #         map(fun-name, collection1, collection2. ......)
 
 # ex 
-l=[1,2,3,4,5]
-def sqr(x):
-    return x**2
-x=map(sqr,l) 
-print(x) 
-print(list(x))  
+# l=[1,2,3,4,5]
+# def sqr(x):
+#     return x**2
+# x=map(sqr,l) 
+# print(x) 
+# print(list(x))  
+
+# ṃultiple list
+l1=[2,4,6,8]
+l2=[1,2,3,6]
+l3=[5,6,7,8]
+def add(x,y,z):
+    return (x+y+z)
+p=map(add,l1,l2,l3)
+print(list(p))
