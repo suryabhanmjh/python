9724c716ad541cff6191b3141a51a90cc4224cc9
Suryabhan Singh <suryabhan283808@gmail.com>
2025-06-21 21:38:20 +0530

    Refactor Subtraction class to implement method overloading for addition and subtraction operations

--- Properties/Polymorphism.py
+++ Properties/Polymorphism.py
@@ -1,18 +1,33 @@
 # function
 
+class Subtraction:
+    def add(self, a, b):
+        return a - b
+    def add(self, a, b, c):
+        return a - b - c
+    def sub(self, *numbers):
+        subt = 0
+        for i in numbers:
+            subt -= i
+        print(subt)
+obj = Subtraction()
+obj.add(10, 5)
+
+
 # method overloading
 
-class Addition:
-    def add(self, a, b):
-        return a + b
-    def add(self, a, b, c):
-        return a + b + c
-    def add(self, a, b, c, d):
-        return a + b + c + d
-    def add(self, *numbers):
-        sum =0 
-        for i in numbers:
-            sum += i
-        print(sum)
-obj = Addition()
-obj.add(1, 2)
+# class Addition:
+#     def add(self, a, b):
+#         return a + b
+#     def add(self, a, b, c):
+#         return a + b + c
+#     def add(self, a, b, c, d):
+#         return a + b + c + d
+#     def add(self, *numbers):
+#         sum =0 
+#         for i in numbers:
+#             sum += i
+#         print(sum)
+# obj = Addition()
+# obj.add(1, 2)
+
